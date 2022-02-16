5caf8d307be67ee8b4fb89b504d9583989239ba0
CoderBig <49120922+dogukanc760@users.noreply.github.com>
2022-02-16 16:56:37 +0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AllExceptionFilter } from 'src/libs/filters/all-exception.filters';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const {httpAdapter} = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new AllExceptionFilter(httpAdapter));
+  // this for global url, for example : localhost:3000/api/
+  app.setGlobalPrefix('api');
+  const config = new DocumentBuilder()
+  .setTitle('Bebeğim Herşey Rest-API')
+  .setDescription('NestJS-MongoDB-JWT Auth-Open API')
+  .setVersion('1.0')
+  .addTag('Api v1 Endpoints')
+  .build();
+const document = SwaggerModule.createDocument(app, config);
+SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
